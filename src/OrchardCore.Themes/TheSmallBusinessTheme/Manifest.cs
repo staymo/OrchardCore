1aad7a966f127b0efee11efa2dcfb68f530e11c6
hmo <staywithmo@163.com>
2019-07-18 12:16:25 +0800

    新增SmallBusinessTheme。从Modern Business复制

---
+++ src/OrchardCore.Themes/TheSmallBusinessTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "SmallBusiness",
+    Author = "Dennis Mo",
+    Website = "",
+    Version = "0.0.1",
+    Description = "SmallBusiness"
+)]

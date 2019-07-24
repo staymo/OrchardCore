d45bcaa1fd489a6c0de8b09f48b195bfbf2194f1
hmo <staywithmo@163.com>
2019-07-24 16:44:36 +0800

    新增Creative Theme

---
+++ src/OrchardCore.Themes/TheCreativeTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Creative Theme",
+    Author = "Dennis Mo",
+    Website = "http://www.ycmusic.cn",
+    Version = "1.0.0",
+    Description = "A theme adapted for our website."
+)]
